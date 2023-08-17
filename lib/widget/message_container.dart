488af6c88a4111dfe3b7e7925adaa3f86127769c
uku <nor10y@yahoo.co.jp>
2023-08-17 20:59:22 +0900

    jump to latest message when refreshed

--- lib/widget/message_container.dart
+++ lib/widget/message_container.dart
@@ -2,23 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:flutter_training_for_spajam/data/chat_message.dart';
 import 'package:flutter_training_for_spajam/widget/message_box.dart';
 
-class MessageContainer extends StatelessWidget {
-  final _controller = ScrollController();
+class MessageContainer extends StatefulWidget {
   final List<ChatMessage> messages;
 
-  MessageContainer({super.key, required this.messages});
+  const MessageContainer({super.key, required this.messages});
 
   @override
-  Widget build(BuildContext context) => Flexible(
-        child: Scrollbar(
+  State<MessageContainer> createState() => _MessageContainerState();
+}
+
+class _MessageContainerState extends State<MessageContainer> {
+  final _controller = ScrollController();
+
+  void goToLast() async {
+    await Future.delayed(const Duration(milliseconds: 350));
+    _controller.jumpTo(_controller.position.maxScrollExtent);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    goToLast();
+    return Flexible(
+      child: Scrollbar(
+        controller: _controller,
+        interactive: true,
+        child: ListView(
           controller: _controller,
-          interactive: true,
-          child: ListView(
-            controller: _controller,
-            children: messages
-                .map((message) => MessageBox(message: message))
-                .toList(),
-          ),
+          children: widget.messages.map((message) => MessageBox(message: message)).toList(),
         ),
-      );
+      ),
+    );
+  }
 }
